53f7926ece08b7b876e21f74915949322fd96f13
Gustavo Rodrigues Lopes Lopes <142946200+Gustagtdev@users.noreply.github.com>
2024-11-03 12:31:07 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var cx=document.getElementById('logo-destaque')
+ var isDragging = false //inicialmente nada está sendo arrastado
+
+ cx.addEventListener('mousedown',function(){
+	 isDragging = true // quando o botão é pressionado começa a andar
+ })
+ 
+ document.addEventListener('mousemove',function(e){
+	 if(isDragging){
+		 cx.style.left = `${e.pageX - 50}px`
+		 cx.style.top = `${e.pageY - 50}px`
+		}
+	})
+	 
+		document.addEventListener('mouseup',function(){
+			isDragging = false //quando o botao do mouse for soltou
+		})
+ 
